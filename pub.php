b785cbd7cefe46a13e10061f77c5be455e68bcfd
ekasit-inthong <ekasit-inthong@hotmail.com>
2017-02-17 23:44:30 +0700

    Update pub.php

--- pub.php
+++ pub.php
@@ -3,7 +3,11 @@
     function pubMqtt($topic,$msg){
         put("https://api.netpie.io/topic/NSETEnergySaving/$topic?retain",$msg);
     }
-    
+
+    function getMqtt($topic){
+        get("https://api.netpie.io/topic/NSETEnergySaving/$topic");
+    }
+
     function getMqttfromlineMsg($lineMsg){
     
         $pos = strpos($lineMsg, ":");
@@ -11,25 +15,42 @@
             $splitMsg = explode(":", $lineMsg);
             $topic = $splitMsg[0];
             $msg = $splitMsg[1];
-            pubMqtt($topic,$msg);
+            //pubMqtt($topic,$msg);
+	    getMqtt($topic);
         }else{
             $topic = "raw";
             $msg = $lineMsg;
-            pubMqtt($topic,$msg);
+            //pubMqtt($topic,$msg);
+	    getMqtt($topic);	
         }
      }
      
      function put($url,$tmsg){
-			      $ch = curl_init($url);
-      			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	      		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+	    $ch = curl_init($url);
+      	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
-	      		curl_setopt($ch, CURLOPT_POSTFIELDS, $tmsg);
-	      		curl_setopt($ch, CURLOPT_USERPWD, "E9hGA0mNypTPQrm:eQLdZGx8csxuHIWkQTWRXQyZK");
+	    curl_setopt($ch, CURLOPT_POSTFIELDS, $tmsg);
+	    curl_setopt($ch, CURLOPT_USERPWD, "E9hGA0mNypTPQrm:eQLdZGx8csxuHIWkQTWRXQyZK");
            
-      			$result = curl_exec($ch);
-	      		curl_close($ch);
-      			return $result;
+      	    $result = curl_exec($ch);
+	    curl_close($ch);
+      	    return $result;
       }
- ?>
+
+     function get($url,$tmsg){
+	    $ch = curl_init($url);
+      	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+	    curl_setopt($ch, CURLOPT_POSTFIELDS, $tmsg);
+	    curl_setopt($ch, CURLOPT_USERPWD, "E9hGA0mNypTPQrm:eQLdZGx8csxuHIWkQTWRXQyZK");
+           
+      	    $result = curl_exec($ch);
+	    curl_close($ch);
+      	    return $result;
+      }
+
+?>
