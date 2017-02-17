5fdae30f79033d31b54601f29071f350956aa907
ekasit-inthong <ekasit-inthong@hotmail.com>
2017-02-17 23:29:21 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -24,7 +24,7 @@ if (!is_null($events['events'])) {
 				'text' => $text
 			];
 
-			$messages = getMqttfromlineMsg($text);
+			//$messages = getMqttfromlineMsg($text);
 			
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
