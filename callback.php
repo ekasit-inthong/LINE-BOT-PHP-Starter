f9db5f5ccd669ee40102b6816ab8a9126c3a43a6
ekasit-inthong <ekasit-inthong@hotmail.com>
2017-02-17 22:34:56 +0700

    Create callback.php

---
+++ callback.php
@@ -0,0 +1,5 @@
+<?php
+require("pub.php");
+$channelId = "1483731860";
+$channelSecret = "982c65ee4140a30796594a205322fa20";
+$mid = "6T6xGizSRdRqxp5i9wZS9hcIPPirBz1jHLLjSdTXgO2RShvaVjPLedPCd3IE7Eq41WDq/zOWA+OHcV/UxemmpqHzugP7DboI9wVHIziRy8r1tLLyeV1EW4Tgf4FOPSFZdy7kSpiJ6j84T02qop4UKAdB04t89/1O/w1cDnyilFU=";
