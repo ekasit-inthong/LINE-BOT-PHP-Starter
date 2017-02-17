3f0236001d33deed84a016cc0c4f3ac8b8dbc032
ekasit-inthong <ekasit-inthong@hotmail.com>
2017-02-17 23:26:33 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,4 +1,6 @@
 <?php
+require("pub.php");
+
 $access_token = '6T6xGizSRdRqxp5i9wZS9hcIPPirBz1jHLLjSdTXgO2RShvaVjPLedPCd3IE7Eq41WDq/zOWA+OHcV/UxemmpqHzugP7DboI9wVHIziRy8r1tLLyeV1EW4Tgf4FOPSFZdy7kSpiJ6j84T02qop4UKAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
@@ -22,6 +24,8 @@ if (!is_null($events['events'])) {
 				'text' => $text
 			];
 
+			$messages = getMqttfromlineMsg($text);
+			
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
