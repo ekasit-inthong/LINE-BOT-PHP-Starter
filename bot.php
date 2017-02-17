407290473bcda78a476c6b4c447008d41f93706b
ekasit-inthong <ekasit-inthong@hotmail.com>
2017-02-17 23:38:43 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,5 +1,5 @@
 <?php
-//require("pub.php");
+require("pub.php");
 
 $access_token = '6T6xGizSRdRqxp5i9wZS9hcIPPirBz1jHLLjSdTXgO2RShvaVjPLedPCd3IE7Eq41WDq/zOWA+OHcV/UxemmpqHzugP7DboI9wVHIziRy8r1tLLyeV1EW4Tgf4FOPSFZdy7kSpiJ6j84T02qop4UKAdB04t89/1O/w1cDnyilFU=';
 
@@ -19,12 +19,13 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
+			getMqttfromlineMsg($text);
 			$messages = [
 				'type' => 'text',
 				'text' => $text
 			];
 
-			//$messages = getMqttfromlineMsg($text);
+			
 			
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
