70795fb612ee8d0709f5a8d98c7b1ead59d500e4
ekasit-inthong <ekasit-inthong@hotmail.com>
2017-02-18 22:40:27 +0700

    Update testnetpie.php

--- testnetpie.php
+++ testnetpie.php
@@ -1,6 +1,7 @@
 <?php
-echo "Hi Test";
+echo "Hi Test welcome to thailand";
 require("pub.php");
 
 $responsemsg = getMqttfromlineMsg("AirCond003/Temperature");
 echo $responsemsg;
+echo "Pass this line alredy";
