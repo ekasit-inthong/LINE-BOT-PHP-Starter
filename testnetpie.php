ce964c704a7d6400fdca4d50c167fab5dc378c7b
ekasit-inthong <ekasit-inthong@hotmail.com>
2017-02-23 22:15:27 +0700

    Update testnetpie.php

--- testnetpie.php
+++ testnetpie.php
@@ -1,7 +1,27 @@
-<?php
-echo "Welcome to thailand 4.0" . "\r\n";
-require("pub.php");
-
-$responsemsg = getMqttfromlineMsg("AirCond003/Temperature");
-echo $responsemsg . "\r\n";
-echo "Pass this line alredy" . "\r\n";
+{
+  "events": [
+    {
+      "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+      "type": "message",
+      "timestamp": 1462629479859,
+      "source": {
+        "type": "user",
+        "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+      },
+      "message": {
+        "id": "325708",
+        "type": "text",
+        "text": "Hello, world"
+      }
+    },
+    {
+      "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+      "type": "follow",
+      "timestamp": 1462629479859,
+      "source": {
+        "type": "user",
+        "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+      }
+    }
+  ]
+}
