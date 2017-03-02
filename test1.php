47f8220df27e4311ac647d5e8069198ae57695a9
ekasit-inthong <ekasit-inthong@hotmail.com>
2017-03-02 21:55:38 +0700

    Update and rename test1 to test1.php

--- test1.php
+++ test1.php
@@ -1,5 +1,4 @@
 <?php
-<?php
 
 $access_token = '6T6xGizSRdRqxp5i9wZS9hcIPPirBz1jHLLjSdTXgO2RShvaVjPLedPCd3IE7Eq41WDq/zOWA+OHcV/UxemmpqHzugP7DboI9wVHIziRy8r1tLLyeV1EW4Tgf4FOPSFZdy7kSpiJ6j84T02qop4UKAdB04t89/1O/w1cDnyilFU=';
 
