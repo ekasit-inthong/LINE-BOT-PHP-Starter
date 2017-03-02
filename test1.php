cc72266505bf63bec91233c4b0a6c100b13663b8
ekasit-inthong <ekasit-inthong@hotmail.com>
2017-03-02 22:18:35 +0700

    Update test1.php

--- test1.php
+++ test1.php
@@ -1,3 +1,4 @@
+
 <?php
 $access_token = '6T6xGizSRdRqxp5i9wZS9hcIPPirBz1jHLLjSdTXgO2RShvaVjPLedPCd3IE7Eq41WDq/zOWA+OHcV/UxemmpqHzugP7DboI9wVHIziRy8r1tLLyeV1EW4Tgf4FOPSFZdy7kSpiJ6j84T02qop4UKAdB04t89/1O/w1cDnyilFU=';
 $url = 'https://api.line.me/v1/oauth/verify';
@@ -28,3 +29,8 @@ echo $obj[1]->payload;
 echo $obj[1]['topic'];
   
 echo "End of File2222";
+
+$json = '["apple","orange","banana","strawberry"]';
+$ar = json_decode($json);
+// access first element of $ar array
+echo $ar[0]; // apple
