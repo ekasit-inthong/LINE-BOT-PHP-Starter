cebfa0c4e48e1e14ecf240458157947740ffdb3f
ekasit-inthong <ekasit-inthong@hotmail.com>
2017-03-02 22:01:36 +0700

    Update test1.php

--- test1.php
+++ test1.php
@@ -1,3 +1,4 @@
+
 <?php
 $access_token = '6T6xGizSRdRqxp5i9wZS9hcIPPirBz1jHLLjSdTXgO2RShvaVjPLedPCd3IE7Eq41WDq/zOWA+OHcV/UxemmpqHzugP7DboI9wVHIziRy8r1tLLyeV1EW4Tgf4FOPSFZdy7kSpiJ6j84T02qop4UKAdB04t89/1O/w1cDnyilFU=';
 $url = 'https://api.line.me/v1/oauth/verify';
@@ -17,3 +18,7 @@ curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
 $text = curl_exec($ch);
 curl_close ($ch);                       
 echo $text . "\r\n";
+
+$obj = json_decode($text, true);
+echo $obj['topic'];
+echo $obj['payload'];
